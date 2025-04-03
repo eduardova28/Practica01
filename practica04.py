ab3b6c152ee2cc52b0a950f86aa7759cf6f2f796
eduardova28 <eduardo.vergaray@utec.edu.pe>
2025-04-03 17:15:11 -0500

    Create practica04.py

---
+++ practica04.py
@@ -0,0 +1,14 @@
+#EJERCICIO 04
+#CALCULAR AREA DE UN RECTANGULO
+def calcularArearectangulo(a,b):
+    area=(a*b)
+    return area
+print("INGRESA BASE")
+a=float(input())
+print("INGRESA ALTURA")
+b=float(input())
+print("EL AREA DEL RECTANGULO ES" + "" +str(calcularArearectangulo(a,b)))
+
+
+
+
